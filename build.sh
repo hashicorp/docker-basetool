870d6d60df8fc09583977162bc6da1b33e57444f
James Phillips <james@hashicorp.com>
2016-02-08 22:52:12 -0800

    Forces CGO off so we get a static binary.

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ fi
 
 # Build a release of the basetool in a hermetic Go build container.
 docker run --rm -v "$PWD":/usr/src/basetool -w /usr/src/basetool \
-       golang:latest sh -c "go get -d -v && go build"
+       golang:latest sh -c "go get -d -v && CGO_ENABLED=0 go build"
 
 # Sign the binary.
 find basetool -type f -exec sh -c 'shasum -a256 $(basename $1) >$1.SHA256SUM' -- {} \;
